99f9a35bcf0f49945871a21ea65f2997f7057512
Arman Hossain Akash <armanhossainakash52@gmail.com>
2023-07-30 11:43:53 +0600

    fast development

---
+++ script.js
@@ -0,0 +1,8 @@
+let mobail_btn= document.getElementById("mobail_btn")
+let main_menu= document.getElementById("main_menu")
+
+mobail_btn.addEventListener("click",()=>{
+  main_menu.classList.toggle("hidden");
+})
+
+// ai khane toggole dara menu on off
